365d1398797b9ec8352a0ddd4de8194c1006c24e
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-17 13:57:23 +0530

    Deletion function added

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -2,15 +2,36 @@ import React, { useEffect, useState } from 'react';
 const Detail = () => {
   const [users, setUsers] = useState([]);
 
-  const getUsersDetail = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/albums');
-    setUsers(await response.json());
-
-  }
-
   useEffect(() => {
     getUsersDetail();
   }, []);
+
+  const getUsersDetail = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+
+  const deleteUser = async (itemId) =>{
+
+    try{
+
+    await fetch(`https://jsonplaceholder.typicode.com/albums/${itemId}`, {
+      method: 'DELETE',
+    });
+    setUsers(users.filter((user) => user.id !== itemId));
+
+    }catch(error){
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -20,7 +41,7 @@ const Detail = () => {
       </div>
       <div className="section-center">
         {
-          users.map((person,index) => {
+          users.map((person, index) => {
             return (
               <div key={index}>
                 <article className="album-item">
@@ -31,11 +52,11 @@ const Detail = () => {
                       <h4 className="identity">Id:{person.id}</h4>
                     </header>
                     <p className="item-text">
-                     <span> title:</span>{person.title}
+                      <span> title:</span>{person.title}
                     </p>
                     <div className="btn-container">
                       <button>update</button>
-                      <button>delete</button>
+                      <button onClick={() => deleteUser(person.id)} >delete</button>
                     </div>
                   </div>
                 </article>
